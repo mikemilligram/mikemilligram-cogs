0f0f58b6f8f10c16f7880963d0912c0d1c3dffd8
Marlon Gundelfinger <marlonqgundelfinger@gmail.com>
2020-06-10 10:00:43 +0200

    username

--- nintendo/nintendo.py
+++ nintendo/nintendo.py
@@ -13,25 +13,26 @@ class Nintendo(commands.Cog):
         self.config.register_member(**default_member)
 
     @commands.group()
-    async def nintendo(self, ctx):
+    async def switch(self, ctx):
         pass
 
-    @nintendo.command(help = 'register or change your nintendo switch code')
+    @switch.command(name='set', help = 'register or change your nintendo switch code')
     async def set_code(self, ctx, code: str):
         await self.config.member_from_ids(ctx.guild.id, ctx.author.id).switchcode.set(code)
         await ctx.send("your switch code has been set")
 
-    @nintendo.command(help = "display your own or someone else's switch code")
+    @switch.command(name='show', help = "display your own or someone else's switch code")
     async def display_code(self, ctx, member: discord.Member = None):
         user = member if member else ctx.author
         code = await self.config.member_from_ids(ctx.guild.id, user.id).switchcode()
         name = user.nick if user.nick else user.name
         await ctx.send(f"{name}'s switch code: " + code)
 
-    @nintendo.command(help = "display the codes of everyone in the server")
+    @switch.command(name='list', help = "display the codes of everyone in the server")
     async def list_codes(self, ctx):
         users = await self.config.all_members(ctx.guild)
         embed = discord.Embed(title = 'switch codes')
         for userid, data in users.items():
-            embed.add_field(name = userid, value = str(data))
+            user = discord.Client.get_user(userid)
+            embed.add_field(name = user.name, value = data['switchcode'])
         await ctx.send(embed=embed)
