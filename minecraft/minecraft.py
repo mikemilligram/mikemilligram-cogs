f49ac2d2076bce2b8c6ef07238057580d780b5f8
Marlon Gundelfinger <marlonqgundelfinger@gmail.com>
2020-08-01 19:44:06 +0200

    test

--- minecraft/minecraft.py
+++ minecraft/minecraft.py
@@ -4,6 +4,9 @@ import discord
 import asyncio
 
 
+DIMENSIONS = {'o': 'overworld', 'n': 'nether', 'e': 'the end'}
+
+
 class Minecraft(commands.Cog):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -11,7 +14,8 @@ class Minecraft(commands.Cog):
         self.config = Config.get_conf(self, identifier=141197)
         # default_member = {}
         default_guild = {
-            'connections': {}
+            'connections': {},
+            'locations': {}
         }
         # self.config.register_member(**default_member)
         self.config.register_guild(**default_guild)
@@ -20,21 +24,21 @@ class Minecraft(commands.Cog):
     async def minecraft(self, ctx):
         pass
 
-    @minecraft.group(aliases=['nether', 'hub'])
-    async def nether_hub(self, ctx):
+    @minecraft.group(aliases=['netherhub'])
+    async def hub(self, ctx):
         pass
 
     @minecraft.group()
-    async def locations(self, ctx):
+    async def location(self, ctx):
         pass
 
     @nether_hub.command()
     async def list(self, ctx):
         async with self.lock:
             connections = await self.config.guild(ctx.guild).connections()
-            embed = discord.Embed(title="NETHER HUB")
+        embed = discord.Embed(title="NETHER HUB")
         for connection, portals in connections.items():
-            embed.add_field(name=connection.title(), value=f"test", inline=True)
+            embed.add_field(name=connection.title(), value=f"\\a", inline=True)
             embed.add_field(name=f"{portals[0]['x']} | {portals[0]['z']}", value="Nether", inline=True)
             embed.add_field(name = f"{portals[1]['x']} | {portals[1]['y']} | {portals[1]['z']}", value = "Overworld",
                             inline = True)
@@ -46,11 +50,33 @@ class Minecraft(commands.Cog):
         nether_portal = {'x': nether_x, 'z': nether_z}
         overworld_portal = {'x': overworld_x, 'y': overworld_y, 'z': overworld_z}
         async with self.lock:
-            data = await self.config.guild(ctx.guild).all()
-        if name.lower() in data['connections'].keys():
+            connections = await self.config.guild(ctx.guild).connections()
+        if name.lower() in connections.keys():
             return await ctx.send("There is already a connection with that name.")
         else:
-            data['connections'][name.lower()] = [nether_portal, overworld_portal]
-        await self.config.guild(ctx.guild).connections.set(data['connections'])
+            connections[name.lower()] = [nether_portal, overworld_portal]
+        await self.config.guild(ctx.guild).connections.set(connections)
         return await ctx.send("The connection has been registered.")
 
+    @location.command()
+    async def list(self, ctx):
+        async with self.lock:
+            locations = await self.config.guild(ctx.guild).locations()
+        embed = discord.Embed(title = "LOCATIONS")
+        for location, coords in locations.items():
+            embed.add_field(name = location.title(), value = "\\a", inline = True)
+            embed.add_field(name = f"{coords['x']} | {coords['y']} | {coords['z']}", value = "Coordinates",
+                            inline = True)
+        return await ctx.send(embed = embed)
+
+    @location.command()
+    async def add(self, ctx, name: str, x: int, y: int, z: int):
+        location = {'x': x, 'y': y, 'z': z}
+        async with self.lock:
+            locations = await self.config.guild(ctx.guild).locations()
+        if name.lower() in locations.keys():
+            return await ctx.send("There is already a location with that name.")
+        else:
+            locations[name.lower()] = location
+        await self.config.guild(ctx.guild).connections.set(locations)
+        return await ctx.send("The location has been registered.")
