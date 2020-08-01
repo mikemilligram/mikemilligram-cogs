b55444e60dd0f989706676964d8a38ceeffff09b
Marlon Gundelfinger <marlonqgundelfinger@gmail.com>
2020-08-01 19:15:40 +0200

    list

--- minecraft/minecraft.py
+++ minecraft/minecraft.py
@@ -31,16 +31,26 @@ class Minecraft(commands.Cog):
     @nether_hub.command()
     async def list(self, ctx):
         async with self.lock:
-            data = await self.config.guild(ctx.guild).all()
+            connections = await self.config.guild(ctx.guild).connections()
+            embed = discord.Embed(title="NETHER HUB")
+        for connection, portals in connections.items():
+            embed.add_field(name=name.title(), inline=True)
+            embed.add_field(name=f"{portals[0]['x']} | {portals[0]['z']}", value="Nether", inline=True)
+            embed.add_field(name = f"{portals[1]['x']} | {portals[1]['y']} | {portals[1]['y']}", value = "Overworld",
+                            inline = True)
+        return await ctx.send(embed=embed)
 
     @nether_hub.command()
-    async def add(self, ctx, name: str, nether_x: int, nether_z: int, overworld_x: int, overworld_y: int, overworld_z: int):
+    async def add(self, ctx, name: str, nether_x: int, nether_z: int, overworld_x: int, overworld_y: int,
+                  overworld_z: int):
         nether_portal = {'x': nether_x, 'z': nether_z}
         overworld_portal = {'x': overworld_x, 'y': overworld_y, 'z': overworld_z}
         async with self.lock:
             data = await self.config.guild(ctx.guild).all()
-        if name in data['connections'].keys():
-            return await ctx.send("There is already a connection with that name")
+        if name.lower() in data['connections'].keys():
+            return await ctx.send("There is already a connection with that name.")
         else:
-            data['connections'][name] = [nether_portal, overworld_portal]
+            data['connections'][name.lower()] = [nether_portal, overworld_portal]
         await self.config.guild(ctx.guild).connections.set(data['connections'])
+        return await ctx.send("The connection has been registered.")
+
