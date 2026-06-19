0074253fcfa1402b24dd8d6dab97bacc4cdd0e0b
Marlon Gundelfinger <marlonqgundelfinger@gmail.com>
2026-06-19 22:08:33 +0200

    generalised call_service command, added light_on, light_off commands, added morse command

--- ha/ha.py
+++ ha/ha.py
@@ -1,8 +1,7 @@
-from typing import Literal
-
 import discord
 from redbot.core import Config, commands, app_commands
 from .rest import HomeAssistantAPI
+
 
 class HomeAssistant(commands.Cog):
     """Home Assistant Cog"""
@@ -36,31 +35,41 @@ class HomeAssistant(commands.Cog):
         if success:
             await self.config.guild(interaction.guild).url.set(url)
             await self.config.guild(interaction.guild).token.set(token)
-            await interaction.response.send_message("Authentication successful. Configuration saved.")
+            await interaction.response.send_message("Authentication successful. Configuration saved.", ephemeral=True)
+            self.api = api
         else:
-            await interaction.response.send_message("Authentication failed.")
+            await interaction.response.send_message("Authentication failed.", ephemeral=True)
             
+    
+    @ha_slash.command(name="reconnect")
+    async def reconnect(self, interaction: discord.Interaction):
+        url = await self.config.guild(interaction.guild).url()
+        token = await self.config.guild(interaction.guild).token()
+        
+        if not url or not token:
+            await interaction.response.send_message("You need to authenticate first using /homeassistant auth.", ephemeral=True)
+            return
+        
+        api = HomeAssistantAPI(url, token)
+        if not api.authenticate():
+            await interaction.response.send_message("Please reauthenticate using /homeassistant auth", ephemeral=True)
+        else:
+            self.api = api
+            await interaction.response.send_message("Successfully reconnected", ephemeral=True)
+    
     
     @ha_slash.command(name="unauth")
     async def unauth(self, interaction: discord.Interaction):
         """Unregister the user's Home Assistant configuration."""
         
         await self.config.guild(interaction.guild).clear()
-        await interaction.response.send_message("Configuration cleared.")
+        await interaction.response.send_message("Configuration cleared.", ephemeral=True)
         
 
     @commands.command(name="service")
     async def call_service(self, ctx: commands.Context, domain: str, entity: str, service: str):
         """Call a service within a specific domain."""
-        
-        url = await self.config.guild(ctx.guild).url()
-        token = await self.config.guild(ctx.guild).token()
-        
-        if not url or not token:
-            await ctx.send("You need to authenticate first using /homeassistant auth.")
-            return
-        
-        api = HomeAssistantAPI(url, token)
+        api = await api_instance(self, ctx)
         
         try:
             api.call_service(domain, entity, service)
@@ -68,30 +77,60 @@ class HomeAssistant(commands.Cog):
 
         except Exception as e:
             await ctx.react_quietly("❌")
+            
 
+    @commands.group(name="light")
+    async def light(self, ctx: commands.Context):
+        """Light control commands."""
+        pass
+    
+    @light.command(name="on")
+    async def light_on(self, ctx: commands.Context, *entities: str):
+        """Turn on one or more light entities."""
+        api = await api_instance(self, ctx)
+        try:
+            api.light_on(entities)
+            await ctx.tick()
 
+        except Exception as e:
+            await ctx.react_quietly("❌")
+        
+    
+    @light.command(name="off")
+    async def light_off(self, ctx: commands.Context, *entities: str):
+        """Turn off one or more light entities."""
+        api = await api_instance(self, ctx)
+        
+        try:
+            api.light_off(entities)
+            await ctx.tick()
+
+        except Exception as e:
+            await ctx.react_quietly("❌")
+        
+        
+    @light.command(name="morse")
+    async def light_morse(self, ctx: commands.Context, message: str, *entities: str):
+        """Send a message in Morse code using the specified light entities."""
+        # api = await api_instance(self, ctx)
+        
+        await self.api.light_morse(message, *entities)
+                    
+            
     @app_commands.checks.has_permissions(administrator=True)
     @ha_slash.command(name="set_satellite", description="Set the device ID for Home Assistant Satellite announcements.")
     async def set_satellite(self, interaction: discord.Interaction, device_id: str):
         """Set the device ID for Home Assistant Satellite announcements."""
         
         await self.config.guild(interaction.guild).entities.set_raw("satellite", value=device_id)
-        await interaction.response.send_message(f"Satellite device ID set to `{device_id}`.")
+        await interaction.response.send_message(f"Satellite device ID set to `{device_id}`.", ephemeral=True)
 
 
     @commands.command(name="say")
     async def say(self, ctx: commands.Context, *, message: str):
         """Announce a message using a media player entity."""
-        
-        url = await self.config.guild(ctx.guild).url()
-        token = await self.config.guild(ctx.guild).token()
         device_id = await self.config.guild(ctx.guild).entities.satellite()
-        
-        if not url or not token:
-            await ctx.send("You need to authenticate first using /homeassistant auth.")
-            return
-        
-        api = HomeAssistantAPI(url, token)
+        api = await api_instance(self, ctx)
         
         try:
             api.announce(message, device_id)
@@ -99,3 +138,15 @@ class HomeAssistant(commands.Cog):
 
         except Exception as e:
             await ctx.react_quietly("❌")
+            
+            
+async def api_instance(ha: HomeAssistant, ctx: commands.Context) -> HomeAssistantAPI:
+    """Helper function to create an API instance with the current configuration."""
+    url = await ha.config.guild(ctx.guild).url()
+    token = await ha.config.guild(ctx.guild).token()
+    
+    if not url or not token:
+        await ctx.send("You need to authenticate first using /homeassistant auth.")
+        return
+    
+    return HomeAssistantAPI(url, token)
--- ha/rest.py
+++ ha/rest.py
@@ -1,5 +1,9 @@
 import requests
-from typing import Any, Dict
+from typing import Any, Dict, List, Literal
+import time
+from .morse import Timings
+import re
+
 
 class HomeAssistantAPI:
   def __init__(self, url: str, token: str):
@@ -13,6 +17,7 @@ class HomeAssistantAPI:
       'Content-Type': 'application/json'
     }
     
+    
   def authenticate(self) -> bool:
     """
     Test authentication by fetching the current user's information.
@@ -23,22 +28,37 @@ class HomeAssistantAPI:
     except requests.RequestException:
       return False
 
-  def call_service(self, domain: str, entity: str, service: str) -> Dict[str, Any]:
+
+  def call_service(self, domain: str, service: str, data: Dict = {}):
     """
     Call a service within a specific domain.
     """
-    
     endpoint = f"{self.endpoint}/services/{domain}/{service}"
-    data = {"entity_id": f"{domain}.{entity}"}
     response = requests.post(endpoint, json=data, headers=self.headers)
     response.raise_for_status()
     return response.json()
+  
+  
+  def light_on(self, entities: List[str]) -> Dict[str, Any]:
+    """
+    Turn on a light entity.
+    """
+    data = {"entity_id": [f"light.{entity}" for entity in entities]}
+    return self.call_service("light", "turn_on", data)
+  
+  
+  def light_off(self, entities: List[str]) -> Dict[str, Any]:
+    """
+    Turn off a light entity.
+    """
+    data = {"entity_id": [f"light.{entity}" for entity in entities]}
+    return self.call_service("light", "turn_off", data) 
+
 
   def announce(self, message: str, device_id: str) -> Dict[str, Any]:
     """
     Announce a message using a media player entity.
     """
-    
     endpoint = f"{self.endpoint}/services/assist_satellite/announce"
     data = {
       "message": message,
@@ -47,3 +67,49 @@ class HomeAssistantAPI:
     response = requests.post(endpoint, json=data, headers=self.headers)
     response.raise_for_status()
     return response.json()
+  
+
+  async def morse_element(self, signal: str = Literal['dot', 'dash'], *entities: str):
+    """
+    Send a Morse code signal to the specified entities.
+    """
+    self.light_on(entities)
+    if signal == 'dot':
+      time.sleep(Timings.DOT_LENGTH)
+    elif signal == 'dash':
+      time.sleep(Timings.DASH_LENGTH)
+    self.light_off(entities)
+
+
+  async def light_morse(self, message: str, *entities: str) -> bool:
+    """Send a message in Morse code using the specified light entities."""
+    
+    if not self.validate_morse(message):
+      return False
+    if '   ' in message or '/' in message:
+      words = re.split(r'   |/', message.strip())
+    else:
+      words = [message]
+    sleep = 0
+    for word in words:
+        if sleep > 0:
+            time.sleep(sleep)
+        sleep = 0
+        for char in word.strip():
+            if sleep > 0:
+                time.sleep(sleep)
+            if char == '.':
+                await self.morse_element('dot', *entities)
+                sleep = Timings.INTRA_LETTER_GAP
+            elif char == '-':
+                await self.morse_element('dash', *entities)
+                sleep = Timings.INTRA_LETTER_GAP
+            elif char == ' ':
+                sleep = Timings.INTER_LETTER_GAP
+        sleep = Timings.INTER_WORD_GAP - Timings.INTRA_LETTER_GAP
+    return True
+                
+                
+  def validate_morse(self, message: str) -> bool:
+    pattern = re.compile(r'^([.-]{1,5}( |   |/| / |$))+$')
+    return bool(pattern.match(message))
